20fc9d7e85b9a80c1409fc3f3d967bc4dc7b1659
Igor K. <podolyanyn@gmail.com>
2025-03-28 15:00:27 +0100

    in views.py I used model Question instead of test data (class Question)

--- polls/views.py
+++ polls/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
+from .models import Question
 
-class Question:
-    def __init__(self, question_id):
-        self.question_text = str(question_id) + '_text'
-        self.id = question_id
-        self.choice_set = ['answer_1', 'answer_2', 'answer_3']
-
-latest_question_list = []
-for i in range(5):
-    latest_question_list.append(Question(i))
+# class Question:
+#     def __init__(self, question_id):
+#         self.question_text = str(question_id) + '_text'
+#         self.id = question_id
+#         self.choice_set = ['answer_1', 'answer_2', 'answer_3']
+#
+# latest_question_list = []
+# for i in range(5):
+#     latest_question_list.append(Question(i))
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
@@ -22,22 +23,15 @@ for i in range(5):
 #     return HttpResponse(output)
 
 def index(request):
-    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
 def detail(request, question_id):
-    # question = get_object_or_404(Question, pk=question_id)
-    # return render(request, "polls/detail.html", {"question": question})
-
-    try:
-        question = latest_question_list[question_id] #Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
