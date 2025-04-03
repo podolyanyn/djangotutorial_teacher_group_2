299f002b9db09062ec7e76c88cf9d38434088d0f
Igor K. <podolyanyn@gmail.com>
2025-04-03 22:05:35 +0200

    Lesson_43_Advanced functionality of HTML... Part 2

--- polls/forms.py
+++ polls/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class QuestionForm(forms.Form):
-    question_text  = forms.CharField(label='Your question', max_length=200)
-    pub_date = forms.DateTimeField(label = 'date published')
+    question_text  = forms.CharField(label='Your question', max_length=200) #, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    pub_date = forms.DateTimeField(label = 'date published', widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
 
